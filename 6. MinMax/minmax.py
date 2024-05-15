e36f62ef6aee967ceb6b691748f5cf3c249c9d41
Harish G <97893600+harishngt@users.noreply.github.com>
2024-05-15 06:39:49 +0530

    Update minmax.py

--- 6. MinMax/minmax.py
+++ 6. MinMax/minmax.py
@@ -1,30 +1,27 @@
-MAX, MIN = 1000, -1000
-
-def minimax(depth, nodeIndex, maximizingPlayer, values, alpha, beta):
-    if depth == 3:
-        return values[nodeIndex]
-    
-    if maximizingPlayer:
-        best = MIN
-        for i in range(0, 2):
-            val = minimax(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta)
-            best = max(best, val)
-            alpha = max(alpha, best)
-            if beta <= alpha:
-                break
-        return best
+import math
+ 
+def minimax (curDepth, nodeIndex,
+             maxTurn, scores, 
+             targetDepth):
+ 
+    # base case : targetDepth reached
+    if (curDepth == targetDepth): 
+        return scores[nodeIndex]
+     
+    if (maxTurn):
+        return max(minimax(curDepth + 1, nodeIndex * 2, 
+                    False, scores, targetDepth), 
+                   minimax(curDepth + 1, nodeIndex * 2 + 1, 
+                    False, scores, targetDepth))
+     
     else:
-        best = MAX
-        for i in range(0, 2):
-            val = minimax(depth + 1, nodeIndex * 2 + i, True, values, alpha, beta)
-            best = min(best, val)
-            beta = min(beta, best)
-            if beta <= alpha:
-                break
-        return best
+        return min(minimax(curDepth + 1, nodeIndex * 2, 
+                     True, scores, targetDepth), 
+                   minimax(curDepth + 1, nodeIndex * 2 + 1, 
+                     True, scores, targetDepth))
 
 if __name__ == "__main__":
-    values = []
+    scores = []
     n = 0
     while n < 6 or n > 8:  
         n = int(input("Enter Number of Players (6-8): "))
@@ -34,7 +31,8 @@ if __name__ == "__main__":
             print("Please enter a number between 6 and 8.")
     for i in range(1, n + 1):  
         x = int(input(f"Player {i}'s Turn: "))
-        values.append(x)
-    win_value = minimax(0, 0, True, values, MIN, MAX)
-    winner_index = values.index(win_value) + 1  
+        scores.append(x)
+    treeDepth = math.log(len(scores), 2)
+    win_value = minimax(0, 0, True, scores,treeDepth)
+    winner_index = scores.index(win_value) + 1  
     print(f"Player {winner_index} wins with choice {win_value}.")
